673e3bd3c7df2a204cd9f78f24853c8789d1ebac
Aleki <aleki.dev@gmail.com>
2016-08-10 10:56:06 +0300

    Replace tabs with spaces Remove all optional semicolons

--- test/test.js
+++ test/test.js
@@ -3,22 +3,22 @@
  * Copyright(c) 2016 Aleki
  * MIT Licensed
  */
-"use strict";
+"use strict"
 
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.TOKEN
 
-const BotFather = require("../index.js");
-const bf = new BotFather(TOKEN);
+const BotFather = require("../index.js")
+const bf = new BotFather(TOKEN)
 
 bf.api("getMe")
   .then((json) => {
     if(json.ok) {
-      const bot = json.result;
-      console.info(`All right! Your @${bot.username} ready for use :)`);
+      const bot = json.result
+      console.info(`All right! Your @${bot.username} ready for use :)`)
     } else {
-      console.error(json.description);
+      console.error(json.description)
     }
   })
   .catch((exception) => {
-    console.error(exception.stack);
-  });
+    console.error(exception.stack)
+  })
